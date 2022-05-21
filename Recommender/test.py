aa4ae6ce84e9cb6d5e9eb73d9d69f140fd995fd8
BenjaminWang236 <ben.lynbrook@gmail.com>
2022-05-20 18:43:48 -0700

    reformatting tab in test.py

--- Recommender/test.py
+++ Recommender/test.py
@@ -1,30 +1,34 @@
-#sudo python3 -m pip install mysql-connector-python
+# sudo python3 -m pip install mysql-connector-python
 
 import mysql.connector
 from mysql.connector import errorcode
 
 try:
-  cnx = mysql.connector.connect(user='admin', password='606HaoYunLai606!', port='3306',
-                              host='database-1.c50spqkkfz7j.us-west-1.rds.amazonaws.com',
-                              database='db')
+    cnx = mysql.connector.connect(
+        user="admin",
+        password="606HaoYunLai606!",
+        port="3306",
+        host="database-1.c50spqkkfz7j.us-west-1.rds.amazonaws.com",
+        database="db",
+    )
 except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
 else:
-  print("Successfully connected to the database")
-  
-  print("Querying data using Connector/Python...")
-  cursor = cnx.cursor()
-  query = ("SELECT * FROM Users")
-  cursor.execute(query)
+    print("Successfully connected to the database")
 
-  for (uid) in cursor: 
-  	print(uid)
+    print("Querying data using Connector/Python...")
+    cursor = cnx.cursor()
+    query = "SELECT * FROM Users"
+    cursor.execute(query)
 
-  print("Closing the connection and cursor...")
-  cursor.close()
-  cnx.close()
+    for uid in cursor:
+        print(uid)
+
+    print("Closing the connection and cursor...")
+    cursor.close()
+    cnx.close()
